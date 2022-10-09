3e06f6e4421bb6d1c15a3b2b1d80763d9a4859e2
RB <1richard.barton@gmail.com>
2022-10-09 22:54:19 +0100

    tested on rpi 3A

---
+++ LED_DMA.py
@@ -0,0 +1,29 @@
+import numpy as np
+import ctypes
+import numpy.ctypeslib as ctl
+import time
+
+rowcount = 1
+colcount = 15
+channels = 1
+leds = rowcount * colcount
+
+dmalib=ctypes.CDLL('/home/pi/led/LED_DMA.so')
+loadleds=dmalib.LED_DMA
+# c program expects:
+#    pointer  to 8 bit uint array
+#    number of LEDs (int)
+loadleds.argtypes = [ctl.ndpointer(np.uint8,flags='aligned,c_contiguous'),ctypes.c_int, ctypes.c_int]
+
+# ================== set up array for c program depending on what to display ================
+for it in range(0,10):
+	for col in range(0,colcount):
+
+		largearray = np.zeros((rowcount, colcount, 3), dtype=np.uint8)  # three 8 bit color
+
+
+		largearray[0,col] = np.array([0, 32,32])
+
+		loadleds(largearray, leds, channels)         # call c program
+		time.sleep(.25)
+print("======================== DONE =========================")
