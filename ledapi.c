3a029736b3971f75f0daf4eab50887e136f2d384
Richard Barton <1richard.barton@gmail.com>
2022-10-12 19:51:01 +0100

    Flush stdout

--- ledapi.c
+++ ledapi.c
@@ -163,6 +163,7 @@ int main(int argc, char *argv[])
         while (dma_active(DMA_CHAN))
             usleep(10);
         fwrite(&msg, 1, 3, stdout);
+        fflush(stdout);
     }
     terminate(0);
     return(0);
